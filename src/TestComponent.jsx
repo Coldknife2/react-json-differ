587baebee143fabc8f9b7eaba5eebec2b1fb7676
Rémi Boussu <remi.boussu@axa.fr>
2022-06-13 15:28:20 +0200

    Init

---
+++ src/TestComponent.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TestComponent = () => {
+    return <h1>Wow</h1>;
+}
+
+export { TestComponent };
---
+++ vite.config.js
@@ -0,0 +1,26 @@
+// vite.config.js
+const path = require('path')
+const { defineConfig } = require('vite')
+
+module.exports = defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.js'),
+      name: 'jsonDiffer',
+      fileName: (format) => 'jsonDiffer.js',
+      formats: ['es']
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['react'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: 'React'
+        }
+      }
+    }
+  }
+})
